7271d822321ea6910c9f4ea64332fe8b5287bda6
zyadhany <zeyad.hany2003@gmail.com>
2024-04-14 15:01:49 +0200

    solved task

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-
 """ module for square opject"""
 
 
@@ -17,7 +16,7 @@ class square():
         """ Area of the square """
         return self.width * self.height
 
-    def PermiterOfMySquare(self):
+    def permiter_of_my_square(self):
         """ Perimeter of my square """
         return (self.width * 2) + (self.height * 2)
 
@@ -31,4 +30,4 @@ if __name__ == "__main__":
     s = square(width=12, height=9)
     print(s)
     print(s.area_of_my_square())
-    print(s.PermiterOfMySquare())
+    print(s.permiter_of_my_square())
