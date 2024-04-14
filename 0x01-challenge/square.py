af70b1ec44e7660a3315ed202f9b7bf5273dece5
zyadhany <zeyad.hany2003@gmail.com>
2024-04-14 15:10:33 +0200

    solved task

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -2,7 +2,7 @@
 """ Module for square opject"""
 
 
-class square():
+class Square():
     """ Square class """
     width = 0
     height = 0
@@ -27,7 +27,7 @@ class square():
 
 if __name__ == "__main__":
     """ Craete an opject """
-    s = square(width=12, height=9)
+    s = Square(width=12, height=9)
     print(s)
     print(s.area_of_my_square())
     print(s.permiter_of_my_square())
