8ac264710a76cbbdcfad7a43b107ce9cbc200026
zyadhany <zeyad.hany2003@gmail.com>
2024-04-14 14:53:18 +0200

    solved task

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -1,12 +1,15 @@
 #!/usr/bin/python3
 
+""" module for square opject"""
+
+
 class square():
-    
+    """ square class """
     width = 0
     height = 0
 
-    
     def __init__(self, *args, **kwargs):
+        """ Instantiation of the class """
         for key, value in kwargs.items():
             setattr(self, key, value)
 
@@ -15,13 +18,16 @@ class square():
         return self.width * self.height
 
     def PermiterOfMySquare(self):
+        """ Perimeter of my square """
         return (self.width * 2) + (self.height * 2)
 
     def __str__(self):
+        """ String representation"""
         return "{}/{}".format(self.width, self.height)
 
-if __name__ == "__main__":
 
+if __name__ == "__main__":
+    """ craete an opject """
     s = square(width=12, height=9)
     print(s)
     print(s.area_of_my_square())
